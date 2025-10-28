fd10f0543608ba01fdfa0d28740d447b978a8f48
RitaJeveaux <ritajeveaux@gmail.com>
2025-10-28 11:13:19 -0300

    rotas funcionais

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductList } from './components/product-list/product-list';
+import { Header } from "./components/header/header";
+import { Footer } from './components/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductList],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('minha-loja-angular');
+  title = 'loja-angular';
 }
--- src/app/pages/product-list/product-list.ts
+++ src/app/pages/product-list/product-list.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductDetail } from '../product-detail/product-detail';
 import { ProductService } from '../../services/product-service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, ProductDetail],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 export class ProductList implements OnInit {
   products: any[] = [];
-  selectedProduct: any;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts()
-  }
-
-  selectProduct(product: any): void {
-    this.selectedProduct = product;
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
   }
 }
